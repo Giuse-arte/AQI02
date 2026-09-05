7bd0594b52afa3156472c8de6d8c822337a2547f
Giuse-arte <giuseppe.artesani@gmail.com>
2026-09-05 09:36:31 +0200

    Update config.js
    Modificato apikey e channel ID

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@
 // Externalized Stations Configuration - Add new stations here effortlessly
 const STATIONS = [
   { id: 2789658, apiKey: '4IMIXLC0RWW9K4PA', name: 'Device N° AQI_01', location: 'Stazione Primaria' },
-  { id: 3412629, apiKey: 'HP8HM37WZ9BAVB4A', name: 'Device N° AQI_02', location: 'Stazione Secondaria' }
+  { id: 3482211, apiKey: 'G704K57NEFAVC0FW', name: 'Device N° AQI_02', location: 'Stazione Secondaria' }
 ];
 
 // EEA (European Environment Agency) Classifications & Thresholds
